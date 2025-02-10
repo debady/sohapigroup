77cd173cee15b6d20d2bd279d0a2cd6ac267077f
some joseph <somejoseph2@gmail.com>
2025-02-10 16:55:49 +0000

    amend

--- includes/details_projet.php
+++ includes/details_projet.php
@@ -1,29 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Détails du projet</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <img src="<?php 
 
-<div class="container">
-    <img src="<?php 
+        if(isset($_GET['nom_image'])){
+            $nom_image = $_GET['nom_image'];
+            echo htmlspecialchars($nom_image); // Sécuriser l'affichage
+        }
+        
+        ?>" class="img-fluid" alt=""><br>
 
-    if(isset($_GET['nom_image'])){
-        $nom_image = $_GET['nom_image'] ;
-        echo $nom_image ;
-    }
-    
-    ?>" class="img-fluid" alt=""><br>
-    <!-- <img src="img/sohapi.jpg" class="img-fluid" alt=""><br> -->
-    <div class="text"><br>
-        <h3>Titre du projet</h3><br>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate tenetur repellat corporis quas
-             nemo at! Dolores cumque quidem dicta est corrupti porro dolorem in corporis recusandae, velit consectetur quis!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate tenetur repellat corporis quas
-             nemo at! Dolores cumque quidem dicta est corrupti porro dolorem in corporis recusandae, velit consectetur quis!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate tenetur repellat corporis quas
-             nemo at! Dolores cumque quidem dicta est corrupti porro dolorem in corporis recusandae, velit consectetur quis!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate tenetur repellat corporis quas
-             nemo at! Dolores cumque quidem dicta est corrupti porro dolorem in corporis recusandae, velit consectetur quis!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate tenetur repellat corporis quas
-             nemo at! Dolores cumque quidem dicta est corrupti porro dolorem in corporis recusandae, velit consectetur quis!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium voluptate tenetur repellat corporis quas
-             nemo at! Dolores cumque quidem dicta est corrupti porro dolorem in corporis recusandae, velit consectetur quis!
-        </p>
+        <div class="text"><br>
+            <?php 
+            if(isset($_GET['contenu'])){
+                $contenu = $_GET['contenu'];
+                echo strip_tags($contenu); // Supprime les balises HTML
+            }
+            ?>
+        </div>
     </div>
-</div>
+</body>
+</html>
--- includes/nos_projets.php
+++ includes/nos_projets.php
@@ -23,7 +23,7 @@
                         <img class="img-fluid w-100" src="https://th.bing.com/th/id/R.b5dff0bce248c0c227a742e7454ca364?rik=%2fKDAb4Lw8bALKQ&pid=ImgRaw&r=0" alt="">
                         <div class="portfolio-overlay">
                             <a class="btn btn-square btn-outline-light mx-1" href="https://th.bing.com/th/id/R.b5dff0bce248c0c227a742e7454ca364?rik=%2fKDAb4Lw8bALKQ&pid=ImgRaw&r=0" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo ''; ?>?nom_image=<?php echo ''; ?>"><i class="fa fa-link"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo 'https://th.bing.com/th/id/R.b5dff0bce248c0c227a742e7454ca364?rik=%2fKDAb4Lw8bALKQ&pid=ImgRaw&r=0'; ?>"><i class="fa fa-link"></i></a>
                         </div>
                     </div>
                     <div class="border border-5 border-light border-top-0 p-4">
@@ -40,7 +40,13 @@
                         <img class="img-fluid w-100" src="https://th.bing.com/th/id/R.b5dff0bce248c0c227a742e7454ca364?rik=%2fKDAb4Lw8bALKQ&pid=ImgRaw&r=0" alt="">
                         <div class="portfolio-overlay">
                             <a class="btn btn-square btn-outline-light mx-1" href="https://th.bing.com/th/id/R.b5dff0bce248c0c227a742e7454ca364?rik=%2fKDAb4Lw8bALKQ&pid=ImgRaw&r=0" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo ''; ?>"><i class="fa fa-link"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo urlencode('https://th.bing.com/th/id/R.b5dff0bce248c0c227a742e7454ca364?rik=%2fKDAb4Lw8bALKQ&pid=ImgRaw&r=0'); ?>&contenu=<?php echo urlencode('  <div class="border border-5 border-light border-top-0 p-4">
+                        <p class="text-primary fw-medium mb-2">Création de Contenus Éducatifs en Santé</p>
+                        <h5 class="lh-base mb-0">Développer des contenus informatifs sous forme de vidéos, 
+                        articles, infographies et podcasts, destinés à fournir des informations précises et accessibles 
+                        sur des sujets médicaux et de prévention, destinés à un large public, incluant les patients et les 
+                        professionnels de santé.</h5>
+                    </div>'); ?>"><i class="fa fa-link"></i></a>
                         </div>
                     </div>
                     <div class="border border-5 border-light border-top-0 p-4">
@@ -58,7 +64,13 @@
                         <img class="img-fluid w-100" src="https://th.bing.com/th/id/R.8873938b929fd048b1394a944a9e890a?rik=4QEka2b%2bkDdPNw&pid=ImgRaw&r=0" alt="">
                         <div class="portfolio-overlay">
                             <a class="btn btn-square btn-outline-light mx-1" href="https://th.bing.com/th/id/R.8873938b929fd048b1394a944a9e890a?rik=4QEka2b%2bkDdPNw&pid=ImgRaw&r=0" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo ''; ?>"><i class="fa fa-link"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo urlencode('https://th.bing.com/th/id/R.8873938b929fd048b1394a944a9e890a?rik=4QEka2b%2bkDdPNw&pid=ImgRaw&r=0'); ?>&contenu=<?php echo urlencode('<div class="border border-5 border-light border-top-0 p-4">
+                        <p class="text-primary fw-medium mb-2">Événements et Conférences en Santé</p>
+                        <h5 class="lh-base mb-0">Organiser des événements, séminaires et conférences, 
+                        en ligne ou en présentiel, pour rassembler des expertsdu domaine médical et de la santé
+                        publique afin de partager des connaissances, promouvoir des innovations et discuter des défis
+                        actuels dans le secteur santé et plus .</h5>
+                    </div>');?>"><i class="fa fa-link"></i></a>
                         </div>
                     </div>
                     <div class="border border-5 border-light border-top-0 p-4">
@@ -76,7 +88,12 @@
                         <img class="img-fluid w-100" src="https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0" alt="">
                         <div class="portfolio-overlay">
                             <a class="btn btn-square btn-outline-light mx-1" href="https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo ''; ?>"><i class="fa fa-link"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo urlencode('https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0'); ?>&contenu=<?php echo urlencode('<div class="border border-5 border-light border-top-0 p-4">
+                        <p class="text-primary fw-medium mb-2">Plateforme de Formation pour les Professionnels de Santé</p>
+                        <h5 class="lh-base mb-0">Concevoir une plateforme en ligne dédiée à la formation continue des
+                         professionnels de santé, leur permettant d\'accéder à des modules de formation sur les dernières 
+                         pratiques médicales, les nouvelles technologies de soins et la gestion des patients.</h5>
+                    </div>'); ?>"><i class="fa fa-link"></i></a>
                         </div>
                     </div>
                     <div class="border border-5 border-light border-top-0 p-4">
@@ -93,7 +110,12 @@
                         <img class="img-fluid w-100" src="https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0" alt="">
                         <div class="portfolio-overlay">
                             <a class="btn btn-square btn-outline-light mx-1" href="https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo ''; ?>"><i class="fa fa-link"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo urlencode('https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0'); ?>&contenu=<?php echo urlencode('<div class="border border-5 border-light border-top-0 p-4">
+                        <p class="text-primary fw-medium mb-2">Consultations Médicales à Distance (Télémédecine)</p>
+                        <h5 class="lh-base mb-0">Mettre en place des services de télémédecine pour permettre aux patients 
+                        d\’accéder à des consultations médicales à distance, offrant ainsi plus d\'accessibilité aux soins, 
+                        notamment dans les zones rurales ou pour les personnes ayant des contraintes .</h5>
+                    </div>');?>"><i class="fa fa-link"></i></a>
                         </div>
                     </div>
                     <div class="border border-5 border-light border-top-0 p-4">
@@ -110,7 +132,13 @@
                         <img class="img-fluid w-100" src="https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0" alt="">
                         <div class="portfolio-overlay">
                             <a class="btn btn-square btn-outline-light mx-1" href="https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo '';?>"><i class="fa fa-link"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="page_detail_projet.php?nom_image=<?php echo urlencode('https://th.bing.com/th/id/R.737af0737cdbc9caa6da77e582be220e?rik=uFmGvMwRMfj8uw&pid=ImgRaw&r=0'); ?>&contenu=<?php echo urlencode('<div class="border border-5 border-light border-top-0 p-4">
+                        <p class="text-primary fw-medium mb-2">Collaboration avec des Établissements de Santé pour Améliorer leur Communication</p>
+                        <h5 class="lh-base mb-0">Aider les hôpitaux et cliniques à optimiser leur communication interne et 
+                        externe, en mettant en place des outils numériques pour la gestion des rendez-vous, des informations
+                         médicales et des échanges entre patients et professionnels.
+                        </h5>
+                    </div>');?>"><i class="fa fa-link"></i></a>
                         </div>
                     </div>
                     <div class="border border-5 border-light border-top-0 p-4">
